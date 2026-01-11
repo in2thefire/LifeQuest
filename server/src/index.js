8b74d9ec591977ad9e7aa34efe2615000cce31e2
Bohdan Kyrylko <in2thefire.code@gmail.com>
2026-01-11 13:15:04 +0100

    Fix auth cookie options and logout

--- server/src/index.js
+++ server/src/index.js
@@ -115,6 +115,7 @@ const cookieOptions = {
   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   secure: process.env.NODE_ENV === "production",
   maxAge: 7 * 24 * 60 * 60 * 1000,
+  path: "/", // важно
 };
 
 const signToken = (user) =>
